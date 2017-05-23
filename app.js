b312f0e21633e00bf60ae1539b2420003db5f5f4
Sompop Kulapalanont <=>
2017-05-23 14:40:09 +0700

    render todo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 angular.module('todo', ['myFactory'])
     .controller('MainController',['todoProvider', function(todoProvider){
-
+        this.todos = todoProvider.getTodos();
     }  ] )
     .controller('AddController',['todoProvider', function(todoProvider){
         
--- factory.js
+++ factory.js
@@ -1,5 +1,14 @@
 angular.module('myFactory', [])
     .factory('todoProvider' ,  () => {
+        var todos = [
+            {"id":123214, "name":"Get Milk", "priority":"Low" },
+            {"id":123215, "name":"Send email", "priority":"High" },
 
-        
+        ];
+        return {
+            getTodos:() => {
+                return todos
+            }
+        }
+
     })
